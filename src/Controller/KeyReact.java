48202c02a1fe6c82dd634c90bc6e855e53601583
ShouTz <ShouTz@users.noreply.github.com>
2019-05-13 16:45:41 +0200

    Sec

---
+++ src/Controller/KeyReact.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+
+public class KeyReact implements EventHandler<KeyEvent>{
+
+	@Override
+	public void handle(KeyEvent event) {
+		
+	}
+
+}
